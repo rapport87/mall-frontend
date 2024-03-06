33ac251ca2f12c28fb187b06efae98869dac943b
rapport87 <rapport87@gmail.com>
2024-03-06 13:26:56 +0900

    url변경

--- src/api.ts
+++ src/api.ts
@@ -6,7 +6,7 @@ const instance = axios.create({
   baseURL: 
     process.env.NODE_ENV === "development" 
     ? "http://127.0.0.1:8000/api/v1/"
-    : "https://learninglab.co.kr:15152/api/v1",
+    : "https://mall.learninglab.co.kr:15152/api/v1",
   withCredentials: true,
 });
 
